37d1c3a9794640f6b85e75f7497ef0dbc6440c75
Stickiler <luke@olympusgaming.org>
2014-03-28 14:19:47 +1000

    Missed NetworkManager.cs

---
+++ 2199/Assets/NetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MasterServer.ipAddress = "localhost";
+		MasterServer.port = 23466;
+		MasterServer.RequestHostList ("2199_Luke");
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			HostData[] data = MasterServer.PollHostList();
+			if (data.Length >= 1)
+			Network.Connect(data[0]);
+			Debug.Log(data);
+			Debug.Log("connected to the Server Hopefully");
+		}
+	}
+
+	[RPC] void setPlayerNumber(int number, int opponent)
+	{
+		GameObject.FindGameObjectWithTag("player_me").tag = "player_" + number;
+		GameObject.FindGameObjectWithTag("player_opponent").tag = "player_" + opponent;
+		Debug.Log("Player Numbers Set");
+	}
+
+	[RPC] void Rotate(int player)
+	{
+		GameObject.FindGameObjectWithTag("player_" + player).GetComponent<Player>().Rotate();
+		Debug.Log("Boards rotated");
+	}
+}
